3b4993f68bf156ca6b5a821826df0f39d7ec131c
Hao1229 <s0939859391@gmail.com>
2020-05-27 18:36:06 +0800

    feat: add introduce page idle popup

--- js/all.js
+++ js/all.js
@@ -9,3 +9,30 @@ if (query.indexOf('introduce') > -1) {
     window.location.href = `${url.protocol}//${url.host}/pages/introduce.html`
   }, 1500)
 }
+
+if (query.indexOf('index') > -1) {
+  setTimeout(() => {
+    window.location.href = `${url.protocol}//${url.host}/index.html`
+  }, 1500)
+}
+
+if (url.pathname.indexOf('introduce') > -1) {
+  idleTimer()
+}
+
+function idleTimer () {
+  let time
+  window.onload = resetTimer
+
+  document.onmousemove = resetTimer
+  document.onkeypress = resetTimer
+
+  function popupShow () {
+    $('#backModal').modal('show')
+  }
+
+  function resetTimer () {
+    clearTimeout(time)
+    time = setTimeout(popupShow, 5000)
+  }
+}
