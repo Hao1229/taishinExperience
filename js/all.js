8e89945ff35fb675aa14f007f298a8ee968fab21
Hao1229 <s0939859391@gmail.com>
2020-05-28 18:04:37 +0800

    fix: transition sometimes will not show problem

--- js/all.js
+++ js/all.js
@@ -1,17 +1,18 @@
 /* 過場跳轉處理 */
 const url = window.location
+const doodle = document.querySelector('css-doodle')
 
-$('#introduce-link').click(function (e) { 
-  e.preventDefault()
-  $('#index').css('display', 'none')
+if (url.pathname.indexOf('pages') > -1) {
+  doodle.use = 'var(--rule)'
   setTimeout(() => {
-    window.location.href = `${url.protocol}//${url.host}/pages/introduce.html`
+    $('.transition-animate').css('z-index', '-1')
   }, 1500)
-})
+}
 
 $('.index-link').click(function (e) { 
   e.preventDefault()
-  $('#introduce').css('display', 'none')
+  doodle.use = 'var(--rule)'
+  $('.transition-animate').css('z-index', '99999')
   setTimeout(() => {
     window.location.href = `${url.protocol}//${url.host}/index.html`
   }, 1500)
