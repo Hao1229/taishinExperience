2e8004cb01f4ee2a8664680fcfb3ccff6fc4607e
Hao1229 <s0939859391@gmail.com>
2020-05-28 18:18:09 +0800

    feat: index add transition

--- js/all.js
+++ js/all.js
@@ -2,19 +2,21 @@
 const url = window.location
 const doodle = document.querySelector('css-doodle')
 
-if (url.pathname.indexOf('pages') > -1) {
-  doodle.use = 'var(--rule)'
-  setTimeout(() => {
-    $('.transition-animate').css('z-index', '-1')
-  }, 1500)
-}
-
 $('.index-link').click(function (e) { 
   e.preventDefault()
   doodle.use = 'var(--rule)'
   $('.transition-animate').css('z-index', '99999')
   setTimeout(() => {
     window.location.href = `${url.protocol}//${url.host}/index.html`
+  }, 1500)
+})
+
+$('#introduce-link').click(function (e) { 
+  e.preventDefault()
+  doodle.use = 'var(--rule)'
+  $('.transition-animate').css('z-index', '99999')
+  setTimeout(() => {
+    window.location.href = `${url.protocol}//${url.host}/pages/introduce.html`
   }, 1500)
 })
 
