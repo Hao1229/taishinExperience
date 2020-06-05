a1cca053f88186fc2a0c30d7c73a3a53a1a8fba7
Hao1229 <s0939859391@gmail.com>
2020-06-05 11:18:02 +0800

    feat: introduce page map left section three add active

---
+++ js/introduce.js
@@ -0,0 +1,5 @@
+$('.introduce__map-land__left-section__three').click(function (e) { 
+  e.preventDefault()
+  $('.introduce__map-land__left-section__three').toggleClass('introduce__map-land__left-section__three__active')
+  $('.introduce__map-land__left-section__three__title').toggleClass('introduce__map-land__left-section__three__title__active')
+})
