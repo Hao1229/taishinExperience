6f7784293285380551c2ac74b4534d705cb34c1a
Hao1229 <s0939859391@gmail.com>
2020-06-05 11:51:57 +0800

    feat: introduce page map left section all add active

--- js/introduce.js
+++ js/introduce.js
@@ -1,5 +1,36 @@
+/* 導覽地圖 active default 切換 */
+
+// 左 3
 $('.introduce__map-land__left-section__three').click(function (e) { 
   e.preventDefault()
   $('.introduce__map-land__left-section__three').toggleClass('introduce__map-land__left-section__three__active')
   $('.introduce__map-land__left-section__three__title').toggleClass('introduce__map-land__left-section__three__title__active')
+})
+
+// 左 2
+$('.introduce__map-land__left-section__two').click(function (e) { 
+  e.preventDefault()
+  $('.introduce__map-land__left-section__two').toggleClass('introduce__map-land__left-section__two__active')
+  $('.introduce__map-land__left-section__two__title').toggleClass('introduce__map-land__left-section__two__title__active')
+})
+
+// 左 1
+$('.introduce__map-land__left-section__one').click(function (e) { 
+  e.preventDefault()
+  $('.introduce__map-land__left-section__one').toggleClass('introduce__map-land__left-section__one__active')
+  $('.introduce__map-land__left-section__one__title').toggleClass('introduce__map-land__left-section__one__title__active')
+})
+
+// 左 4
+$('.introduce__map-land__left-section__four').click(function (e) { 
+  e.preventDefault()
+  $('.introduce__map-land__left-section__four').toggleClass('introduce__map-land__left-section__four__active')
+  $('.introduce__map-land__left-section__four__title').toggleClass('introduce__map-land__left-section__four__title__active')
+})
+
+// 左 5
+$('.introduce__map-land__left-section__five').click(function (e) { 
+  e.preventDefault()
+  $('.introduce__map-land__left-section__five').toggleClass('introduce__map-land__left-section__five__active')
+  $('.introduce__map-land__left-section__five__title').toggleClass('introduce__map-land__left-section__five__title__active')
 })
