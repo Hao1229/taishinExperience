6ad91d042d269c4988f665bb99ac66734ed3c4b0
Hao1229 <s0939859391@gmail.com>
2020-06-07 16:42:06 +0800

    feat: introduce page map add hover change index

--- js/introduce.js
+++ js/introduce.js
@@ -7,12 +7,26 @@ $('.introduce__map-land__left-section__three').click(function (e) {
   $('.introduce__map-land__left-section__three__title').toggleClass('introduce__map-land__left-section__three__title__active')
 })
 
+$('.introduce__map-land__left-section__three').hover(function () {
+    $('.introduce__map-land__left-section__three').css('z-index', '100')
+  }, function () {
+    $('.introduce__map-land__left-section__three').css('z-index', 'initial')
+  }
+)
+
 // 左 2
 $('.introduce__map-land__left-section__two').click(function (e) { 
   e.preventDefault()
   $('.introduce__map-land__left-section__two').toggleClass('introduce__map-land__left-section__two__active')
   $('.introduce__map-land__left-section__two__title').toggleClass('introduce__map-land__left-section__two__title__active')
 })
+
+$('.introduce__map-land__left-section__two').hover(function () {
+    $('.introduce__map-land__left-section__two').css('z-index', '100')
+  }, function () {
+    $('.introduce__map-land__left-section__two').css('z-index', 'initial')
+  }
+)
 
 // 左 1
 $('.introduce__map-land__left-section__one').click(function (e) { 
@@ -21,6 +35,13 @@ $('.introduce__map-land__left-section__one').click(function (e) {
   $('.introduce__map-land__left-section__one__title').toggleClass('introduce__map-land__left-section__one__title__active')
 })
 
+$('.introduce__map-land__left-section__one').hover(function () {
+    $('.introduce__map-land__left-section__one').css('z-index', '100')
+  }, function () {
+    $('.introduce__map-land__left-section__one').css('z-index', 'initial')
+  }
+)
+
 // 左 4
 $('.introduce__map-land__left-section__four').click(function (e) { 
   e.preventDefault()
@@ -28,12 +49,26 @@ $('.introduce__map-land__left-section__four').click(function (e) {
   $('.introduce__map-land__left-section__four__title').toggleClass('introduce__map-land__left-section__four__title__active')
 })
 
+$('.introduce__map-land__left-section__four').hover(function () {
+    $('.introduce__map-land__left-section__four').css('z-index', '100')
+  }, function () {
+    $('.introduce__map-land__left-section__four').css('z-index', 'initial')
+  }
+)
+
 // 左 5
 $('.introduce__map-land__left-section__five').click(function (e) { 
   e.preventDefault()
   $('.introduce__map-land__left-section__five').toggleClass('introduce__map-land__left-section__five__active')
   $('.introduce__map-land__left-section__five__title').toggleClass('introduce__map-land__left-section__five__title__active')
 })
+
+$('.introduce__map-land__left-section__five').hover(function () {
+    $('.introduce__map-land__left-section__five').css('z-index', '100')
+  }, function () {
+    $('.introduce__map-land__left-section__five').css('z-index', 'initial')
+  }
+)
 
 // 右 4
 $('.introduce__map-land__right-section__four').click(function (e) { 
