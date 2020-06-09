dc8c01b6ddfdf8508b22896e760bad34b2acd413
Hao1229 <s0939859391@gmail.com>
2020-06-09 19:46:55 +0800

    feat: modify door animation

--- js/all.js
+++ js/all.js
@@ -33,26 +33,35 @@ function idleTimer () {
 function closeDoor (target) {
   $('.right-door').addClass('right-door__active')
   $('.left-door').addClass('left-door__active')
-
   setTimeout(() => {
-    window.location.href = `${url.protocol}//${url.host}/pages/${target}.html`
-  }, 2000)
+    $('.transition-space').css('display', 'block')
+    openDoor(target)
+  }, 500)
 }
 
-function openDoor () {
+function openDoor (target) {
   $('.right-door').removeClass('right-door__active')
   $('.left-door').removeClass('left-door__active')
+  setTimeout(() => {
+    window.location.href = `${url.protocol}//${url.host}/pages/${target}.html`
+  }, 500)
 }
 
 if (url.pathname.indexOf('introduce') > -1) {
   setTimeout(() => {
-    openDoor()
-  }, 500)
-  setTimeout(() => {
     $('.transition-space').addClass('transition-space__none')
     watchTranslate()
-  }, 4000)
+  }, 3000)
 }
+
+// if (url.pathname.indexOf('index') > -1) { // 避免跳轉回首頁會出現門
+//   $('.right-door').css('opacity', '0')
+//   $('.left-door').css('opacity', '0')
+//   setTimeout(() => {
+//     $('.right-door').css('opacity', '1')
+//     $('.left-door').css('opacity', '1')
+//   }, 2000)
+// }
 
 function watchTranslate () {
   const translate = document.querySelector('.transition-space__none')
